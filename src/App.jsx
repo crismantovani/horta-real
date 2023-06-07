684e1b56ab0d0986fd4ef7740d1762176b1f6c38
Cristina Mantovani <cristina.mantovani@accenture.com>
2023-06-06 21:21:03 -0300

    feat: :sparkles: ADD Header
    ADDING header component to the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Outlet } from "react-router-dom";
 
+import Header from './components/Header/Header';
 import Toolbar from './components/Toolbar/Toolbar';
+
 import './App.css'
 
-
 function App () {
-  return (<>
-  <Toolbar></Toolbar>
-  </>);
+  const [titlePage, setTitlePage] = useState("Condominio Burgues");
+
+  return (
+    <>
+      <div>
+        <Header></Header>
+        <div className='pageTitle'>{titlePage}</div>
+      </div>
+      <Outlet />
+      <Toolbar></Toolbar>
+    </>
+  );
 }
 
 export default App;
