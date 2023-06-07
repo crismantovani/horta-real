7a013529a3635291642dcd08972596983e2dcfe4
Cristina Mantovani <cristina.mantovani@accenture.com>
2023-06-06 22:38:34 -0300

    feat: :poop: ADD Card Component to the Home Page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
 import "./Home.css";
+import Card from "../../components/Card/Card";
 
 const Home = () => {
   return (
     <>
-      <h1>Página Home</h1>
+      <article>
+        <Card></Card>
+      </article>
     </>
   );
 };
